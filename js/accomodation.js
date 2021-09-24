469e8218138cf992db4b5381522a27730cdadd5c
carlos <carlos.kipkoech@student.moringaschool>
2021-09-24 04:33:32 +0300

    add delay

--- js/accomodation.js
+++ js/accomodation.js
@@ -15,14 +15,14 @@ function bookingForm() {
   var message = document.getElementById("message").value;
 
   if (name !== "" && email !== "" && message !== "") {
-    $(".sucess").fadeIn().delay(1500).fadeOut(1000);
+    $(".sucess").fadeIn().delay(2000).fadeOut(1000);
     $(".sucess").text(
       "Thank you " +
         name +
         " we have received your Booking Request. Check your email to confirm your Booking and proceed with payment."
     );
   } else if (name == "" || email == "" || message == "") {
-    $(".fail").fadeIn().delay(1500).fadeOut(1000);
+    $(".fail").fadeIn().delay(2000).fadeOut(1000);
     $(".fail").text(
       "Fill in your details to proceed with Booking and dont forget to drop a comment,This will help us serve  you BETTER"
     );
