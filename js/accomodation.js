7aa313d3c7db83bfb4359f0782439cb663b77920
carlos <carlos.kipkoech@student.moringaschool>
2021-09-24 04:06:12 +0300

    edit navbar

--- js/accomodation.js
+++ js/accomodation.js
@@ -1,31 +1,34 @@
+$(document).ready(function () {
+  $(".sucess").hide();
+  $(".fail").hide();
+  $(".portofolio").click(function () {
+    $(".tog").toggle(2000);
+    $(".togOut").toggle(2000);
+  });
+});
 
- $(document).ready(function(){
-     $(".portofolio").click(function(){
-         $(".tog").toggle(2000);
-         $(".togOut").toggle(2000);
+function bookingForm() {
+  //  function bookingFormFunction(){
 
-     })
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
 
- })  
- 
- function bookingForm(){
+  if (name !== "" && email !== "" && message !== "") {
+    $(".sucess").fadeIn().delay(1500).fadeOut(1000);
+    $(".sucess").text(
+      "Thank you " +
+        name +
+        " we have received your Booking Request. Check your email to confirm your Booking and proceed with payment."
+    );
+  } else if (name == "" || email == "" || message == "") {
+    $(".fail").fadeIn().delay(1500).fadeOut(1000);
+    $(".fail").text(
+      "Fill in your details to proceed with Booking and dont forget to drop a comment,This will help us serve  you BETTER"
+    );
+  }
+}
 
-    //  function bookingFormFunction(){
-
-        var name=document.getElementById('name').value;
-        var email=document.getElementById('email').value;
-        var message=document.getElementById('message').value;
-    
-        if (name !== "" && email !== "" && message !== ""){
-            alert('Thank you ' + name + ' we have received your Booking Request. Check your email to confirm your Booking and proceed with payment.');
-        } 
-        
-        else if(name !== "" || email !== "" || message !== ""){
-            alert('Fill your details to proceed with Booking and dont forget to drop a comment ...This will help us serve u BETTER..!!!')
-        }
-     }
-
-     document.getElementById("output").innerHTML = bookingFormFunction();
+document.getElementById("output").innerHTML = bookingFormFunction();
 
 // };
-
